ea697efaa59c0630431515e67f0ae2d8f7aec23c
Tumofeu <tumofeu@gmail.com>
2017-05-08 20:36:59 +0400

    Add collider for one tree Add jump Add ambient sounds Minor terrain update

--- Source/Assets/Scripts/PlayerControllerScript.cs
+++ Source/Assets/Scripts/PlayerControllerScript.cs
@@ -11,14 +11,17 @@ public class PlayerControllerScript : MonoBehaviour
     public int cameraSensitivity;
     public Camera playerCamera;
 
-    float rotationX = 0F;
+    //float rotationX = 0F;
     float rotationY = 0F;
-    Quaternion originalRotation;
+    //Quaternion originalRotation;
+
+    float distToGround;
 
     // Use this for initialization
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
+        distToGround = GetComponent<Collider>().bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -34,7 +37,12 @@ public class PlayerControllerScript : MonoBehaviour
 
     private void ProcessMovement()
     {
-        rigid.MovePosition(transform.position + (Input.GetAxis("Vertical") * transform.forward + Input.GetAxis("Horizontal") * transform.right).normalized * speedModifier*Time.deltaTime);
+        rigid.MovePosition(transform.position + (Input.GetAxis("Vertical") * transform.forward + Input.GetAxis("Horizontal") * transform.right).normalized * speedModifier * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rigid.AddForce(transform.up * 300, ForceMode.Impulse);
+        }
     }
 
     private void ProcessRotation()
@@ -46,5 +54,10 @@ public class PlayerControllerScript : MonoBehaviour
 
         playerCamera.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
 
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
     }
 }
